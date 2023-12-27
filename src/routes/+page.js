b0c9f1b1932f403d226d837cb500d4a477d9560c
Peter E Conn <conn.peter@gmail.com>
2023-12-27 19:20:18 +0000

    Make prerenderable.

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,7 @@
 import { csvToArr } from "$lib";
 
+export const prerender = true;
+
 export async function load({ fetch, params }) {
     const response = await fetch('CRIMPS.csv');
 	const contents = await response.text();
